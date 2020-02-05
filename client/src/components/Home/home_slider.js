85e10ccdb3278f43b0e3d9fc788af3a257117dd2
mohak92 <tamhane.mohak@gmail.com>
2020-02-05 12:11:52 -0500

    rendering home screen

--- client/src/components/Home/home_slider.js
+++ client/src/components/Home/home_slider.js
@@ -1,64 +1,72 @@
 import React from 'react';
 import Slider from 'react-slick';
-import ButtonOne from '../utils/button';
+import MyButton from '../utils/button';
 
 const HomeSlider = (props) => {
 
     const slides = [
         {
-            img:'/images/gadgetHome.jpg',
-            lineOne:'Smartphone',
-            lineTwo:'iPhone XS',
-            linkTitle:'Shop now',
-            linkTwo:'/shop?categ=iPhone XS'
+            img:'/images/featured/featured_home.jpg',
+            lineOne:'NVIDIA RTX 2080Ti',
+            lineTwo:'Custom rental shop',
+            linkTitle:'Rent now',
+            linkTo:'/shop'
         },
         {
-            img:'/images/gadgetHome2.jpg',
-            lineOne:'Xbox One',
-            lineTwo:'Huge discounts',
+            img:'/images/featured/featured_home_2.jpg',
+            lineOne:'iPhone 11 Pro',
+            lineTwo:'Awesome discounts',
             linkTitle:'View offers',
-            linkTwo:'/shop'
+            linkTo:'/shop'
         }
     ]
-    
+
     const settings = {
         dots: false,
-        infinite: true, 
+        infinite: true,
         speed: 500,
         slidesToShow: 1,
         slidesToScroll: 1,
-        arrow: false 
+        arrows: false
     }
 
-
-   const generateSlides = () => (
+    const generateSlides = () => (
         slides ?
-        slides.map((item,i) =>(
-            <div key={i}>
-            <div className='featured_image'>
-                style={{
-                    background:`url(${item.img})`,
-                    height:`${window.innerHeight}px`
-                }}
-                <div className="featured_action">
-            <div className="tag title">{item.lineOne}</div>
-            <div className="tag low_title">{item.lineTwo}</div>
-
+            slides.map((item,i)=>(
+                <div key={i}>
+                    <div className="featured_image"
+                        style={{
+                            background:`url(${item.img})`,
+                            height:`${window.innerHeight}px`
+                        }}
+                    >
+                        <div className="featured_action">
+                            <div className="tag title">{item.lineOne}</div>
+                            <div className="tag low_title">{item.lineTwo}</div>
+                            <div>
+                                <MyButton
+                                    type="default"
+                                    title={item.linkTitle}
+                                    linkTo={item.linkTo}
+                                    addStyles={{
+                                        margin:'10px 0 0 0'
+                                    }}
+                                />
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            </div>
-            </div>
-        ))
+            ))
         :null
     )
-
-
+    
+ 
 
     return (
-        <div className='featured_container'>
+        <div className="featured_container">
             <Slider {...settings}>
                 { generateSlides()}
             </Slider>
-            
         </div>
     );
 };
