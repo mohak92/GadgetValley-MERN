58b7e61c07bcbcadaf1e4b65306a551de8cf51c3
Shehryar Ehtesham <se318@rutgers.edu>
2020-02-10 11:41:40 -0500

    Updated products_actions.js

--- client/src/actions/products_actions.js
+++ client/src/actions/products_actions.js
@@ -3,7 +3,8 @@ import {
     GET_PRODUCTS_BY_SELL,
     GET_PRODUCTS_BY_ARRIVAL,
     GET_BRANDS,
-    GET_CATEGORIES
+    GET_CATEGORIES,
+    GET_PRODUCTS_TO_SHOP
 } from './types';
 
 import { PRODUCT_SERVER } from '../components/utils/misc';
@@ -29,6 +30,32 @@ export function getProductsByArrival(){
         payload: request
     }
 }
+
+export function getProductsToShop(skip, limit, filters = [], previousState = []){
+
+    const data = {
+        limit,
+        skip,
+        filters
+    }
+
+    const request = axios.post(`${PRODUCT_SERVER}/shop`, data)
+        .then(response => {
+            return {
+                size: response.data.size,
+                articles: response.data.articles
+            }
+        });
+
+        return {
+            type: GET_PRODUCTS_TO_SHOP,
+            payload: request
+        }
+}
+
+
+
+
 
 export function getBrands(){
 
