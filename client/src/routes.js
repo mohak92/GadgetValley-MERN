95517040e40e8007db150dcb84a40c1c4ad113e0
Mohak Tamhane <tamhane.mohak@gmail.com>
2020-02-24 12:35:04 -0500

    route to update user profile

--- client/src/routes.js
+++ client/src/routes.js
@@ -12,6 +12,7 @@ import UserDashboard from "./components/User";
 import AddProduct from "./components/User/Admin/add_product";
 import ManageCategories from './components/User/Admin/manage_categories';
 import UserCart from './components/User/cart';
+import UpdateProfile from './components/User/update_profile';
 
 const Routes = () => {
   return (
@@ -20,6 +21,7 @@ const Routes = () => {
         <Route path="/user/dashboard" exact component={Auth(UserDashboard, true)} />
         <Route path="/admin/add_product" exact component={Auth(AddProduct, true)} />
         <Route path="/user/cart" exact component={Auth(UserCart,true)}/>
+        <Route path="/user/user_profile" exact component={Auth(UpdateProfile,true)}/>
         <Route path="/admin/manage_categories" exact component={Auth(ManageCategories,true)}/>
 
         <Route path="/product_detail/:id" exact component={Auth(ProductPage,null)}/>
