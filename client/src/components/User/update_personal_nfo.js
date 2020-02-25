839fd16adb737ec333ec9ea1ba5ebd3ed99d96f4
Shehryar Ehtesham <se318@rutgers.edu>
2020-02-25 02:28:20 -0500

    update personal info page

---
+++ client/src/components/User/update_personal_nfo.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import FormField from '../utils/Form/formfield';
+
+
+class UpdatePersonalNfo extends Component {
+    render() {
+        return (
+            <div>
+                info
+            </div>
+        );
+    }
+}
+
+export default UpdatePersonalNfo;
